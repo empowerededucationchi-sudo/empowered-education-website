867bead1b691d0c8f568b08ee090be87eb0e68d4
empowerededucationchi-sudo <empowerededucationchi@gmail.com>
2026-05-21 18:38:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 window.EE_CONFIG = {
-  airtableToken: 'patzZtoFXxWMTXCqP.272a81d0c8329acd32d164d2a9bc729a5ceff6691335301f00bf85c86a5cc82b',
+  airtableToken: 'paty19mGQlnnhwEtr.f9bafd6084439a1106e9a25b5b2cc9a1e50b6d085a962d6c50f1a782f436f1fb',
   baseId: 'appjLJL9DCLZCrss5',
+  tables: {
+    student:  'Table 1',
+    mentor:   'Table 1',
+    workshop: 'Table 1',
+    shadow:   'Table 1',
+  }
 };
